53f5d89d3651d8e90b25aec084d767e7445d7ca4
tanisk jha <jhatanishqq@outlook.com>
2020-01-27 17:49:30 +0530

    last commit

---
+++ Day1.js
@@ -0,0 +1,22 @@
+// Declare a constant variable, , and assign it the value Math.PI. You will not pass this challenge unless the variable is declared as a constant and named PI (uppercase).
+
+// Read a number, , denoting the radius of a circle from stdin.
+
+// Use  and  to calculate the  and  of a circle having radius.
+
+// Print  as the first line of output and print  as the second line of output.
+
+function main() {
+
+
+    var radius = readLine();
+
+    const PI = Math.PI;
+
+    console.log(PI * radius * radius)
+    
+
+    console.log(2 * PI * radius) }
+
+
+    
