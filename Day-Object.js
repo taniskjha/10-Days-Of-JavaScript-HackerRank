b8766930bed6640ae618674874dc080196b445d2
tanisk jha <jhatanishqq@outlook.com>
2020-01-28 19:31:01 +0530

    last commit

---
+++ Day-Object.js
@@ -0,0 +1,13 @@
+function Rectangle(a, b) {
+    
+    var rect = { 
+        length:a,
+        width:b,
+        perimeter:2 * (a + b),
+        area: a * b
+           
+    }
+    
+    return rect
+    
+    }  Rectangle(4, 5)
