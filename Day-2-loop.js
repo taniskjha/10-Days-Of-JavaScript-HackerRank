7071fb393a37e9a12fdc70c2dd551f487c6b3b45
tanisk jha <jhatanishqq@outlook.com>
2020-01-28 12:39:26 +0530

    Last commit

---
+++ Day-2-loop.js
@@ -0,0 +1,26 @@
+function vowelsAndConsonants(s) 
+{ 
+    
+    // print vowels
+
+    var vowels = ['a', 'e', 'i', 'o', 'u']
+    for (var i = 0; i < s.length; i++)
+        {
+            if (vowels.indexOf(s[i]) > -1)
+                {
+                    console.log(s[i]);
+                }
+        }
+
+   // print consonant     
+    
+    for (var j = 0; j < s.length; j++)
+        {
+            if (vowels.indexOf(s[j]) < 0)
+                {
+                    console.log(s[j]);
+                }
+        }
+    
+}
+    vowelsAndConsonants('javascriptloops');
